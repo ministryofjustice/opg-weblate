e3aa78d2045c68a6769331e3a29812b08aa41280
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-01-26 10:55:02 +0000

    initialise terraform

---
+++ terraform/environment/terraform.tf
@@ -0,0 +1,46 @@
+terraform {
+  backend "s3" {
+    bucket         = "opg.terraform.state"
+    key            = "opg-weblate-environment/terraform.tfstate"
+    encrypt        = true
+    region         = "eu-west-1"
+    role_arn       = "arn:aws:iam::311462405659:role/opg-weblate-ci"
+    dynamodb_table = "remote_lock"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.51.0"
+    }
+  }
+  required_version = ">= 1.2.2"
+}
+
+variable "default_role" {
+  type    = string
+  default = "opg-weblate-ci"
+}
+
+provider "aws" {
+  alias  = "eu_west_1"
+  region = "eu-west-1"
+  default_tags {
+    tags = local.default_tags
+  }
+  assume_role {
+    role_arn     = "arn:aws:iam::${local.environment.account_id}:role/${var.default_role}"
+    session_name = "opg-weblate-terraform-session"
+  }
+}
+
+provider "aws" {
+  alias  = "management_eu_west_1"
+  region = "eu-west-1"
+  default_tags {
+    tags = local.default_tags
+  }
+  assume_role {
+    role_arn     = "arn:aws:iam::311462405659:role/${var.default_role}"
+    session_name = "opg-weblate-terraform-session"
+  }
+}
