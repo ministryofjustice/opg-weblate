ee09e9d110b993f0feb50938bffcad3c01934932
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-01-27 11:06:17 +0000

    refactor structure to use a folder per persistent environment

---
+++ terraform/environment/pr_environment/pr_environment.auto.tfvars
@@ -0,0 +1,4 @@
+account_id        = "653761790766"
+account_name      = "development"
+is_production     = false
+container_version = "latest"
--- terraform/environment/pr_environment/variables.tf
+++ terraform/environment/pr_environment/variables.tf
@@ -1,32 +1,36 @@
-variable "environments" {
-  type = map(
-    object({
-      account_id        = string
-      account_name      = string
-      is_production     = bool
-      container_version = string
-      }
-    )
-  )
+variable "account_id" {
+  type = string
 }
+
+variable "is_production" {
+  type = bool
+}
+
+variable "container_version" {
+  type = string
+
+}
+variable "account_name" {
+  type = string
+}
+
 
 locals {
   environment_name = lower(replace(terraform.workspace, "_", "-"))
-  environment      = contains(keys(var.environments), local.environment_name) ? var.environments[local.environment_name] : var.environments["default"]
 
   mandatory_moj_tags = {
     business-unit    = "OPG"
     application      = "opg-weblate"
     environment-name = local.environment_name
     owner            = "OPG Webops: opgteam+weblate@digital.justice.gov.uk"
-    is-production    = local.environment.is_production
+    is-production    = var.is_production
     runbook          = "https://github.com/ministryofjustice/opg-weblate"
     source-code      = "https://github.com/ministryofjustice/opg-weblate"
   }
 
   optional_tags = {
     infrastructure-support = "OPG Webops: opgteam+opg-weblate@digital.justice.gov.uk"
-    account-name           = local.environment.account_name
+    account-name           = var.account_name
   }
 
   default_tags = merge(local.mandatory_moj_tags, local.optional_tags)
