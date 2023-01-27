94b64f0af729f6499336609cf68c0f2112141b9d
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-01-27 17:05:14 +0000

    update workspace

---
+++ terraform/environment/pr_environments/main.tf
@@ -0,0 +1,15 @@
+module "weblate" {
+  source = "../../modules/weblate"
+
+  application_log_retention_days  = 30
+  ecs_capacity_provider           = "FARGATE_SPOT"
+  weblate_repository_url          = "weblate/weblate"
+  weblate_container_version       = var.container_version
+  alb_deletion_protection_enabled = false
+
+  providers = {
+    aws.region            = aws.eu_west_1
+    aws.global            = aws.global
+    aws.management_global = aws.management_global
+  }
+}
