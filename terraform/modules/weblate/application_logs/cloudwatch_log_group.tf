b81528b82bf0ae368064e9a74f6f55c8788e735b
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-01-27 17:38:30 +0000

    fix log group name

--- terraform/modules/weblate/application_logs/cloudwatch_log_group.tf
+++ terraform/modules/weblate/application_logs/cloudwatch_log_group.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "application_logs" {
-  name              = "${data.aws_default_tags.current.tags.environment-name}_application_logs"
+  name              = "${data.aws_default_tags.current.tags.application}-${data.aws_default_tags.current.tags.environment-name}-application-logs"
   retention_in_days = var.application_log_retention_days
   provider          = aws.region
 }
