dbe9680e8d32f662e9b3a16abddf8245c6afea39
Tom Gulliver <tgulliver97@gmail.com>
2023-01-30 12:01:54 +0000

    MPLAB-632 - provision certificate

---
+++ terraform/shared/production/terraform.tf
@@ -0,0 +1,58 @@
+terraform {
+  backend "s3" {
+    bucket         = "opg.terraform.state"
+    key            = "opg-weblate-shared-production/terraform.tfstate"
+    encrypt        = true
+    region         = "eu-west-1"
+    role_arn       = "arn:aws:iam::311462405659:role/opg-weblate-ci"
+    dynamodb_table = "remote_lock"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.51.0"
+    }
+  }
+  required_version = ">= 1.2.2"
+}
+
+variable "default_role" {
+  type    = string
+  default = "opg-weblate-ci"
+}
+
+provider "aws" {
+  alias  = "eu_west_1"
+  region = "eu-west-1"
+  default_tags {
+    tags = local.default_tags
+  }
+  assume_role {
+    role_arn     = "arn:aws:iam::679638075911:role/${var.default_role}"
+    session_name = "opg-weblate-terraform-session"
+  }
+}
+
+provider "aws" {
+  alias  = "global"
+  region = "us-east-1"
+  default_tags {
+    tags = local.default_tags
+  }
+  assume_role {
+    role_arn     = "arn:aws:iam::679638075911:role/${var.default_role}"
+    session_name = "opg-weblate-terraform-session"
+  }
+}
+
+provider "aws" {
+  alias  = "management_eu_west_1"
+  region = "eu-west-1"
+  default_tags {
+    tags = local.default_tags
+  }
+  assume_role {
+    role_arn     = "arn:aws:iam::311462405659:role/${var.default_role}"
+    session_name = "opg-weblate-terraform-session"
+  }
+}
