532c3e193bc02d5c28cc7e9e57f1e437712c62a0
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-02-07 09:39:14 +0000

    fix name of secrets passed into ecs module

--- terraform/modules/weblate/ecs.tf
+++ terraform/modules/weblate/ecs.tf
@@ -34,9 +34,9 @@ module "app" {
     postgres_password              = aws_secretsmanager_secret.app_secrets["postgres_password"].arn
     redis_password                 = aws_secretsmanager_secret.app_secrets["redis_password"].arn
     weblate_email_host_password    = aws_secretsmanager_secret.app_secrets["weblate_email_host_password"].arn
-    weblate_github_username        = data.aws_secretsmanager_secret.shared_secrets["weblate_gpg_identity"].arn
-    weblate_github_username        = data.aws_secretsmanager_secret.shared_secrets["weblate_github"].arn
-    weblate_social_auth_github_key = data.aws_secretsmanager_secret.shared_secrets["weblate_social_auth_github"].arn
+    weblate_gpg_identity        = data.aws_secretsmanager_secret.shared_secrets["weblate_gpg_identity"].arn
+    weblate_github        = data.aws_secretsmanager_secret.shared_secrets["weblate_github"].arn
+    weblate_social_auth_github = data.aws_secretsmanager_secret.shared_secrets["weblate_social_auth_github"].arn
   }
   weblate_repository_url          = var.weblate_repository_url
   weblate_container_version       = var.weblate_container_version
